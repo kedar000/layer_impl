516d66f2e0bdb9eb12364318acb123caea7cc8e6
kedar000 <140879080+kedar000@users.noreply.github.com>
2026-05-15 11:07:32 +0530

    added : new fiels

--- controller/UploadController.java
+++ controller/UploadController.java
@@ -1,13 +1,35 @@
-package com.example.questionvalidator.controller;
+package org.examplemodulespringboot.imple_layer_and_model.controller;
 
+import org.examplemodulespringboot.imple_layer_and_model.dto.ExtractedQuestionsResponse;
+import org.examplemodulespringboot.imple_layer_and_model.model.QuestionData;
+import org.examplemodulespringboot.imple_layer_and_model.service.FileExtractionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class UploadController {
 
-    @GetMapping("/health")
-    public String health() {
-        return "Project Running";
+    @Autowired
+    private FileExtractionService fileExtractionService;
+
+    @PostMapping("/upload")
+    public ExtractedQuestionsResponse uploadFile(
+            @RequestParam("file") MultipartFile file
+    ) throws Exception {
+
+        List<QuestionData> questions =
+                fileExtractionService.extractQuestions(file);
+
+        ExtractedQuestionsResponse response =
+                new ExtractedQuestionsResponse();
+
+        response.setQuestions(questions);
+        response.setTotalQuestions(questions.size());
+
+        return response;
     }
 }
